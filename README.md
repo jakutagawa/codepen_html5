1603dbc957554783594a9e1822933ae07b7efa5f
Jon Akutagawa <20073293+jakutagawa@users.noreply.github.com>
2018-10-14 23:44:11 -0700

    rewrote photo display and engaged movie into canvas

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # codepen_html5
+
+----------------------
+Fork of an [edited Pen by Sarah Frost](https://github.com/sarahmfrost/codepen_html5).
+
+This repo was created for **UCSC DANM 220** - Introduction to Programming for Artists.
--- functions.js
+++ functions.js
@@ -1,153 +1,198 @@
 /////
 $(function() {
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
-  var video = document.getElementById('video');
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    var video = document.getElementById('video');
 });
 
 //////////
+var photo_dict = {
+    1: 'https://i1.wp.com/film-grab.com/wp-content/uploads/2015/01/6131.jpg',
+    2: 'https://i0.wp.com/film-grab.com/wp-content/uploads/2015/01/6230.jpg',
+    3: 'https://i2.wp.com/film-grab.com/wp-content/uploads/2015/01/6330.jpg',
+    4: 'https://i0.wp.com/film-grab.com/wp-content/uploads/2015/01/6428.jpg'
+}
+
+var random_bttf_image = new Image;
+
+random_bttf_image.src = photo_dict[Math.floor((Math.random() * 4) + 1)];
+
+random_bttf_image.onload = function() {
+    canvas.width = random_bttf_image.width * .5;
+    canvas.height = random_bttf_image.height * .5;
+}
+
 var canvas = document.getElementsByTagName('canvas')[0];
-    canvas.width = 400;
-canvas.height = 600;
 
-    var sarah_id = new Image;
+alert("Jon's version of HTML5 Canvas Context to selectively zoom in and out.\n" +
+    "Drag to move image around canvas. Click to zoom at that location. Shift-click to zoom out. Mousewheel up/down over the canvas to zoom in to/out from that location.\n" +
+    "Also added handling for videos and audio files. ");
 
+window.onload = function() {
 
-    window.onload = function(){
+    var ctx = canvas.getContext('2d');
+    trackTransforms(ctx);
 
-            var ctx = canvas.getContext('2d');
-            trackTransforms(ctx);
+    function redraw() {
 
-    function redraw(){
+        // Clear the entire canvas
+        var p1 = ctx.transformedPoint(0, 0);
+        var p2 = ctx.transformedPoint(canvas.width, canvas.height);
+        ctx.clearRect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
 
-          // Clear the entire canvas
-          var p1 = ctx.transformedPoint(0,0);
-          var p2 = ctx.transformedPoint(canvas.width,canvas.height);
-          ctx.clearRect(p1.x,p1.y,p2.x-p1.x,p2.y-p1.y);
+        ctx.save();
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.restore();
 
-          ctx.save();
-          ctx.setTransform(1,0,0,1,0,0);
-          ctx.clearRect(0,0,canvas.width,canvas.height);
-          ctx.restore();
-
-          ctx.drawImage(sarah_id,0,0);
-
-        }
-        redraw();
-
-      var lastX=canvas.width/2, lastY=canvas.height/2;
-
-      var dragStart,dragged;
-
-      canvas.addEventListener('mousedown',function(evt){
-          document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
-          lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
-          lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
-          dragStart = ctx.transformedPoint(lastX,lastY);
-          dragged = false;
-      },false);
-
-      canvas.addEventListener('mousemove',function(evt){
-          lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
-          lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
-          dragged = true;
-          if (dragStart){
-            var pt = ctx.transformedPoint(lastX,lastY);
-            ctx.translate(pt.x-dragStart.x,pt.y-dragStart.y);
-            redraw();
-                }
-      },false);
-
-      canvas.addEventListener('mouseup',function(evt){
-          dragStart = null;
-          if (!dragged) zoom(evt.shiftKey ? -1 : 1 );
-      },false);
-
-      var scaleFactor = 1.1;
-
-      var zoom = function(clicks){
-          var pt = ctx.transformedPoint(lastX,lastY);
-          ctx.translate(pt.x,pt.y);
-          var factor = Math.pow(scaleFactor,clicks);
-          ctx.scale(factor,factor);
-          ctx.translate(-pt.x,-pt.y);
-          redraw();
-      }
-
-      var handleScroll = function(evt){
-          var delta = evt.wheelDelta ? evt.wheelDelta/40 : evt.detail ? -evt.detail : 0;
-          if (delta) zoom(delta);
-          return evt.preventDefault() && false;
-      };
-
-      canvas.addEventListener('DOMMouseScroll',handleScroll,false);
-      canvas.addEventListener('mousewheel',handleScroll,false);
-    };
-
-    sarah_id.src = 'https://static1.squarespace.com/static/587605301b631b0b57d35dab/t/5bb7b05ec83025fc2848f871/1538764894917/sarah_schoolID.JPG';
-
-    // Adds ctx.getTransform() - returns an SVGMatrix
-    // Adds ctx.transformedPoint(x,y) - returns an SVGPoint
-    function trackTransforms(ctx){
-      var svg = document.createElementNS("http://www.w3.org/2000/svg",'svg');
-      var xform = svg.createSVGMatrix();
-      ctx.getTransform = function(){ return xform; };
-
-      var savedTransforms = [];
-      var save = ctx.save;
-      ctx.save = function(){
-          savedTransforms.push(xform.translate(0,0));
-          return save.call(ctx);
-      };
-
-      var restore = ctx.restore;
-      ctx.restore = function(){
-        xform = savedTransforms.pop();
-        return restore.call(ctx);
-              };
-
-      var scale = ctx.scale;
-      ctx.scale = function(sx,sy){
-        xform = xform.scaleNonUniform(sx,sy);
-        return scale.call(ctx,sx,sy);
-              };
-
-      var rotate = ctx.rotate;
-      ctx.rotate = function(radians){
-          xform = xform.rotate(radians*180/Math.PI);
-          return rotate.call(ctx,radians);
-      };
-
-      var translate = ctx.translate;
-      ctx.translate = function(dx,dy){
-          xform = xform.translate(dx,dy);
-          return translate.call(ctx,dx,dy);
-      };
-
-      var transform = ctx.transform;
-      ctx.transform = function(a,b,c,d,e,f){
-          var m2 = svg.createSVGMatrix();
-          m2.a=a; m2.b=b; m2.c=c; m2.d=d; m2.e=e; m2.f=f;
-          xform = xform.multiply(m2);
-          return transform.call(ctx,a,b,c,d,e,f);
-      };
-
-      var setTransform = ctx.setTransform;
-      ctx.setTransform = function(a,b,c,d,e,f){
-          xform.a = a;
-          xform.b = b;
-          xform.c = c;
-          xform.d = d;
-          xform.e = e;
-          xform.f = f;
-          return setTransform.call(ctx,a,b,c,d,e,f);
-      };
-
-      var pt  = svg.createSVGPoint();
-      ctx.transformedPoint = function(x,y){
-          pt.x=x; pt.y=y;
-          return pt.matrixTransform(xform.inverse());
-      }
-
+        ctx.drawImage(random_bttf_image, (canvas.width - random_bttf_image.width) / 2, (canvas.height - random_bttf_image.height) / 2);
 
     }
+    redraw();
+
+    var lastX = canvas.width / 2,
+        lastY = canvas.height / 2;
+
+    var dragStart, dragged;
+
+    canvas.addEventListener('mousedown', function(evt) {
+        document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
+        lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
+        lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
+        dragStart = ctx.transformedPoint(lastX, lastY);
+        dragged = false;
+    }, false);
+
+    canvas.addEventListener('mousemove', function(evt) {
+        lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
+        lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
+        dragged = true;
+        if (dragStart) {
+            var pt = ctx.transformedPoint(lastX, lastY);
+            ctx.translate(pt.x - dragStart.x, pt.y - dragStart.y);
+            redraw();
+        }
+    }, false);
+
+    canvas.addEventListener('mouseup', function(evt) {
+        dragStart = null;
+        if (!dragged) zoom(evt.shiftKey ? -1 : 1);
+    }, false);
+
+    // increased zoom when clicked
+    var scaleFactor = 5;
+
+    var zoom = function(clicks) {
+        var pt = ctx.transformedPoint(lastX, lastY);
+        ctx.translate(pt.x, pt.y);
+        var factor = Math.pow(scaleFactor, clicks);
+        ctx.scale(factor, factor);
+        ctx.translate(-pt.x, -pt.y);
+        redraw();
+    }
+
+    var handleScroll = function(evt) {
+        var delta = evt.wheelDelta ? evt.wheelDelta / 40 : evt.detail ? -evt.detail : 0;
+        if (delta) zoom(delta);
+        return evt.preventDefault() && false;
+    };
+
+    canvas.addEventListener('DOMMouseScroll', handleScroll, false);
+    canvas.addEventListener('mousewheel', handleScroll, false);
+
+
+    var cw = Math.floor(canvas.clientWidth / 100);
+    var ch = Math.floor(canvas.clientHeight / 100);
+    //canvas.width = cw;
+    //canvas.height = ch;
+
+    video.addEventListener('play', function() {
+        draw(video, ctx, canvas.width, canvas.height);
+    }, false);
+
+
+    function draw(v, c, w, h) {
+        if (v.paused || v.ended) {
+            random_bttf_image.src = photo_dict[Math.floor((Math.random() * 4) + 1)];
+            return false;
+        }
+        c.drawImage(v, 0, 0, w, h);
+        setTimeout(draw, 20, v, c, w, h);
+    }
+};
+
+
+
+// Adds ctx.getTransform() - returns an SVGMatrix
+// Adds ctx.transformedPoint(x,y) - returns an SVGPoint
+function trackTransforms(ctx) {
+    var svg = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+    var xform = svg.createSVGMatrix();
+    ctx.getTransform = function() {
+        return xform;
+    };
+
+    var savedTransforms = [];
+    var save = ctx.save;
+    ctx.save = function() {
+        savedTransforms.push(xform.translate(0, 0));
+        return save.call(ctx);
+    };
+
+    var restore = ctx.restore;
+    ctx.restore = function() {
+        xform = savedTransforms.pop();
+        return restore.call(ctx);
+    };
+
+    var scale = ctx.scale;
+    ctx.scale = function(sx, sy) {
+        xform = xform.scaleNonUniform(sx, sy);
+        return scale.call(ctx, sx, sy);
+    };
+
+    var rotate = ctx.rotate;
+    ctx.rotate = function(radians) {
+        xform = xform.rotate(radians * 180 / Math.PI);
+        return rotate.call(ctx, radians);
+    };
+
+    var translate = ctx.translate;
+    ctx.translate = function(dx, dy) {
+        xform = xform.translate(dx, dy);
+        return translate.call(ctx, dx, dy);
+    };
+
+    var transform = ctx.transform;
+    ctx.transform = function(a, b, c, d, e, f) {
+        var m2 = svg.createSVGMatrix();
+        m2.a = a;
+        m2.b = b;
+        m2.c = c;
+        m2.d = d;
+        m2.e = e;
+        m2.f = f;
+        xform = xform.multiply(m2);
+        return transform.call(ctx, a, b, c, d, e, f);
+    };
+
+    var setTransform = ctx.setTransform;
+    ctx.setTransform = function(a, b, c, d, e, f) {
+        xform.a = a;
+        xform.b = b;
+        xform.c = c;
+        xform.d = d;
+        xform.e = e;
+        xform.f = f;
+        return setTransform.call(ctx, a, b, c, d, e, f);
+    };
+
+    var pt = svg.createSVGPoint();
+    ctx.transformedPoint = function(x, y) {
+        pt.x = x;
+        pt.y = y;
+        return pt.matrixTransform(xform.inverse());
+    }
+
+
+}
